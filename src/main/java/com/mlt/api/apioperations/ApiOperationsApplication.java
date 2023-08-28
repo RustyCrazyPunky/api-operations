13690de956ed3539d63aea9bde75dcc220aa12d8
RustyCrazyPunky <crazypunky@protonmail.com>
2023-08-28 17:51:11 -0300

    Init

---
+++ src/main/java/com/mlt/api/apioperations/ApiOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.mlt.api.apioperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlt/api/apioperations/ApiOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlt.api.apioperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
